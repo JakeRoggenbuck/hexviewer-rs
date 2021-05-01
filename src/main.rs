b9b0a9ae8ec30691a80489adda6c642641698877
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-01 14:28:57 -0700

    Change around some functions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::io;
-use std::io::SeekFrom;
-use std::io::prelude::*;
 use std::fs::File;
-
+use std::io;
+use std::io::prelude::*;
+use std::io::SeekFrom;
 
 trait Hex {
     fn down(&mut self);
     fn up(&mut self);
-    fn read_hex(&mut self, file: File);
+    fn read_hex(&mut self, file: &mut File);
 }
 
 struct HexReader {
     index: u64,
-    buffer: [u8; 160]
+    buffer: Vec::<u8>,
 }
 
 impl Hex for HexReader {
@@ -24,17 +23,23 @@ impl Hex for HexReader {
         self.index -= 16;
     }
 
-    fn read_hex(&mut self, file: File) {
+    fn read_hex(&mut self, file: &mut File) {
         file.seek(SeekFrom::Start(self.index));
         file.read(&mut self.buffer);
 
-        println!("{:X?}", self.buffer[..100]);
+        println!("{:X?}", self.buffer);
     }
 }
 
 fn main() -> io::Result<()> {
     let mut file = File::open("file.txt")?;
 
+    let mut hex_reader = HexReader {
+        index: 0,
+        buffer: Vec::new(),
+    };
+
+    hex_reader.read_hex(&mut file);
 
     Ok(())
 }
