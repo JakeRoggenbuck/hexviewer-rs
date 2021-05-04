bf8238c440b696c75bb81976d268e272169ad9df
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-03 21:19:57 -0700

    Fix warning and read until end

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,33 @@ use std::io::SeekFrom;
 trait Hex {
     fn down(&mut self);
     fn up(&mut self);
-    fn read_hex(&mut self, file: &mut File);
+    fn read_hex(&mut self, file: &mut File) -> io::Result<()>;
 }
 
 struct HexReader {
     index: u64,
+    file_length: u64,
     buffer: Vec::<u8>,
 }
 
 impl Hex for HexReader {
     fn down(&mut self) {
-        self.index += 16;
+        if self.index < self.file_length - 16 {
+            self.index += 16;
+        }
     }
 
     fn up(&mut self) {
-        self.index -= 16;
+        if self.index > 16 {
+            self.index -= 16;
+        }
     }
 
-    fn read_hex(&mut self, file: &mut File) {
-        file.seek(SeekFrom::Start(self.index));
-        file.read(&mut self.buffer);
-
+    fn read_hex(&mut self, file: &mut File) -> io::Result<()> {
+        file.seek(SeekFrom::Start(self.index))?;
+        file.take(80).read_to_end(&mut self.buffer)?;
         println!("{:X?}", self.buffer);
+        Ok(())
     }
 }
 
@@ -36,10 +41,12 @@ fn main() -> io::Result<()> {
 
     let mut hex_reader = HexReader {
         index: 0,
+        file_length: file.metadata().unwrap().len(),
         buffer: Vec::new(),
     };
 
-    hex_reader.read_hex(&mut file);
-
+    hex_reader.read_hex(&mut file)?;
+    hex_reader.up();
+    hex_reader.read_hex(&mut file)?;
     Ok(())
 }
